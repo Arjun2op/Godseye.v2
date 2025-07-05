a9320592aaa8c9286c4acc59a2f8123d44539bc5
Arjun2op <105331201+Arjun2op@users.noreply.github.com>
2025-07-05 21:06:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -21,17 +21,15 @@ apt-get update && apt-get install -y \
     libgtk-3-0 \
     fonts-liberation \
     libappindicator3-1 \
-    xdg-utils
-
-# Install Chrome using webdriver-manager (simplest approach)
-pip install webdriver-manager
-
-# Set environment variables
-export CHROME_BIN="/usr/bin/google-chrome"
-export CHROMEDRIVER_PATH="/usr/local/bin/chromedriver"
+    xdg-utils \
+    google-chrome-stable
 
 # Install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# Make sure the Chrome binary is in PATH
+ln -s /usr/bin/google-chrome /usr/local/bin/chrome
 
 # Run the application
 python3 main.py
